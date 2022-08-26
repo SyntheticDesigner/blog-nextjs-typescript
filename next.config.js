c309e529ec2884d7ec9b71dcc4e79df7005f813e
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-08-25 17:12:32 -0700

    testing vercel env variables

--- next.config.js
+++ next.config.js
@@ -8,23 +8,23 @@ const nextConfig = (phase) => {
     return {
       reactStrictMode: true,
       swcMinify: true,
-      env: {
-        mongodb_username: "andrew",
-        mongodb_password: "SqUMjraDdR4bJb7A",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "test-blog",
-      },
+      // env: {
+      //   mongodb_username: "andrew",
+      //   mongodb_password: "SqUMjraDdR4bJb7A",
+      //   mongodb_clustername: "cluster0",
+      //   mongodb_database: "test-blog",
+      // },
     };
   }
   return {
     reactStrictMode: true,
     swcMinify: true,
-    env: {
-      mongodb_username: "andrew",
-      mongodb_password: "SqUMjraDdR4bJb7A",
-      mongodb_clustername: "cluster0",
-      mongodb_database: "blog",
-    },
+    // env: {
+    //   mongodb_username: "andrew",
+    //   mongodb_password: "SqUMjraDdR4bJb7A",
+    //   mongodb_clustername: "cluster0",
+    //   mongodb_database: "blog",
+    // },
   };
 };
 
